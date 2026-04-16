3e0479a1a62cdd146022e743dd810daee174803e
Masaki1152 <takanenoharusaku@gmail.com>
2026-04-16 10:16:59 +0900

    feat:便利なコンポーネントの追加

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primaryColor = Colors.lightBlue;
+  static const white = Colors.white;
+  static const black = Colors.black;
   static const errorColor = Colors.red;
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,1 +1,2 @@
 export 'app_colors.dart';
+export 'error_handler.dart';
