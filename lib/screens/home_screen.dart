80bd13f79738244d7af8e6f48963075075e32de2
Masaki1152 <takanenoharusaku@gmail.com>
2026-04-16 10:44:33 +0900

    feat:ホーム画面への遷移を確認

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.white,
+      backgroundColor: AppColors.primaryColor,
       body: Column(
         children: [
 
