bda28da543881d1adf61976b6f51e8acb7cf86e7
Masaki1152 <takanenoharusaku@gmail.com>
2026-04-16 14:36:34 +0900

    feature:ホーム画面にアプリバージョンの表示

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,57 +7,76 @@ import 'package:more_enjoy_karaoke_life/conf/constantDev.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static const String appVersion = String.fromEnvironment(
+      'APP_VERSION', defaultValue: 'v0.0.0');
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: AppColors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 60),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset(
-                  ConstantDev.appHomeIconPath,
-                  width: screenWidth * 0.8,
-                  fit: BoxFit.contain,
-                ),
-                const SizedBox(height: 60),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(
-                        height: 80,
-                        child: CommonButton(
-                          text: t.common.start,
-                          onPressed: () {
-                            print('');
-                          },
-                        ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 60),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset(
+                      ConstantDev.appHomeIconPath,
+                      width: screenWidth * 0.8,
+                      fit: BoxFit.contain,
+                    ),
+                    const SizedBox(height: 60),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 100),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          SizedBox(
+                            height: 80,
+                            child: CommonButton(
+                              text: t.common.start,
+                              onPressed: () {
+                                print('');
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                            height: 80,
+                            child: CommonButton(
+                              text: t.common.aboutApp,
+                              onPressed: () {
+                                print('');
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 24),
-                      SizedBox(
-                        height: 80,
-                        child: CommonButton(
-                          text: t.common.aboutApp,
-                          onPressed: () {
-                            print('');
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          const Positioned(
+            right: 20,
+            bottom: 20,
+            child: Text(
+              appVersion,
+              style: TextStyle(
+                color: AppColors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,5 +4,6 @@ class AppColors {
   static const primaryColor = Colors.lightBlue;
   static const white = Colors.white;
   static const black = Colors.black;
+  static const grey = Colors.grey;
   static const errorColor = Colors.red;
 }
